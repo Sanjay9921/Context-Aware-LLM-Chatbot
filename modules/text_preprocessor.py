29f0dc2f964b2f755f0bd92394a56b2c69bfca6b
Sanjay9921 <sanjay.prabhu1729@gmail.com>
2025-05-04 18:21:46 +0200

    English library fix

--- modules/text_preprocessor.py
+++ modules/text_preprocessor.py
@@ -1,8 +1,15 @@
 # modules/text_preprocessor.py
 
 import spacy
+from spacy.cli import download
 
-nlp = spacy.load("en_core_web_sm") # English Language
+try:
+    # Try loading the model
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    # If model is missing, download it
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def preprocess_text(text):
     doc = nlp(text)
