b01f87f4c7b7c8aa75496079fd0240038db6eb4b
Sanjay9921 <sanjay.prabhu1729@gmail.com>
2025-05-04 20:05:40 +0200

    Code Remaster

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from modules.pdf_parser import extract_text_from_pdf
 from modules.text_preprocessor import preprocess_text
 from modules.qa_engine import generate_answer
 
-st.set_page_config(page_title="Context Aware PDF Chatbot")
+st.set_page_config(page_title="Context Aware PDF Chatbot", layout="wide")
 st.title("Chat with your PDF :)")
 
-uploaded_file = st.file_uploader("Upload a PDF", type="pdf")
+uploaded_file = st.file_uploader("Upload a PDF", type=["pdf"])
 
 if uploaded_file:
     text = extract_text_from_pdf(uploaded_file)
@@ -21,4 +21,5 @@ if uploaded_file:
     if user_question:
         with st.spinner("Generating answer..."):
             answer = generate_answer(full_context, user_question)
-            st.write("**Answer:**", answer)
+        st.markdown("** Answer:**")
+        st.write(answer)
--- modules/text_preprocessor.py
+++ modules/text_preprocessor.py
@@ -1,15 +1,10 @@
 # modules/text_preprocessor.py
 
 import spacy
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    import subprocess
-    import sys
-    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"], check=True)
-    nlp = spacy.load("en_core_web_sm")
+
+nlp = spacy.load("en_core_web_sm")
     
 def preprocess_text(text):
     doc = nlp(text)
-    chunks = [sent.text.strip() for sent in doc.sents if len(sent.text.strip()) > 20]
+    chunks = [(ent.text, ent.label_) for ent in doc.ents]
     return chunks
